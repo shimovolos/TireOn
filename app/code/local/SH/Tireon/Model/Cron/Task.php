1a0917ff2b39a36cce4bc4c46dbdea8240368d8a
shimovolos <shimovolos.aa.09@gmail.com>
2016-03-01 20:31:29 +0300

    remove some csv migrations, remove checkout password

--- app/code/local/SH/Tireon/Model/Cron/Task.php
+++ app/code/local/SH/Tireon/Model/Cron/Task.php
@@ -14,9 +14,9 @@ class SH_Tireon_Model_Cron_Task
         $files = array(
             SH_Tireon_Model_CSV::CSV_FILE_NAME_TYRES,
             SH_Tireon_Model_CSV::CSV_FILE_NAME_WHEELS,
-            SH_Tireon_Model_CSV::CSV_FILE_NAME_INDUSTRIAL_TYRES,
-            SH_Tireon_Model_CSV::CSV_FILE_NAME_TRUCK_TYRES,
-            SH_Tireon_Model_CSV::CSV_FILE_NAME_OTHER,
+//            SH_Tireon_Model_CSV::CSV_FILE_NAME_INDUSTRIAL_TYRES,
+//            SH_Tireon_Model_CSV::CSV_FILE_NAME_TRUCK_TYRES,
+//            SH_Tireon_Model_CSV::CSV_FILE_NAME_OTHER,
         );
 
         foreach($files as $file) {
